112648868f4e241df02495714985a200d9df8f11
Paweł Domalewski <info@fakt.com.pl>
2014-03-23 23:14:03 +0100

    wersja linux drukuje znaki (nie rusza kolorow)

--- weze.cpp
+++ weze.cpp
@@ -177,7 +177,10 @@ void SetTekstKolor(short kolor)
 void PutZnak(char* znak)
 {
   unsigned long count;
-
+  char txt[2];
+  txt[0] = znak[0];
+  txt[1] = '\0';
+  puts(txt);
 //  WriteConsole(hStdOut, znak, 1, &count, NULL);
 } // PutZnak
 
